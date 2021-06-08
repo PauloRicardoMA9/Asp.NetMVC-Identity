8f3c22de022bb86ad8b4aeb7461e2c5987f304b7
Paulo Ricardo <paulo.silva@ma9.com.br>
2021-06-08 14:56:17 -0300

    Primeiro commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using AspNetCoreIdentity.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreIdentity.Controllers
+{
+    [Authorize] // Só pode acessar quem está logado
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [AllowAnonymous] // Permite o acesso sem estar logado
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")] // Precisa de uma permissão especial para acessar
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
